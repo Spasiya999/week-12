cb6a22f21049974cc9d8fea5e44e9b6e9043bce6
Sachintha Pasindu <spsd49@gmail.com>
2023-06-11 11:50:21 +0530

    create tables for booking

---
+++ database/migrations/2023_06_11_044503_create_rooms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('room_number', 10)->unique();
+            $table->string('room_type', 10);
+            $table->text('description');
+            $table->integer('beds');
+            $table->integer('occupancy');
+            $table->decimal('price_per_day', 10, 2);
+            $table->string('status', 10)->default('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
