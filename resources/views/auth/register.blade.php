4dcebbb7e354c65b7370717e85c5961f6f25caf8
Sachintha Pasindu <spsd49@gmail.com>
2023-06-11 09:49:08 +0530

    add phone number and address

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,6 +16,20 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <!-- phone Number -->
+        <div class="mt-4">
+            <x-input-label for="phone" :value="__('Phone')" />
+            <x-text-input id="phone" class="block mt-1 w-full" type="tel" name="phone" :value="old('phone')" required autocomplete="phone" />
+            <x-input-error :messages="$errors->get('phone')" class="mt-2" />
+        </div>
+
+        <!-- Address -->
+        <x-input-label class="mt-4">
+            <x-input-label for="address"/>Address</x-input-label>
+            <x-text-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required autocomplete="address" />
+            <x-input-error :messages="$errors->get('address')" class="mt-2" />
+        </div>
+
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
